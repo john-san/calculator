701c58490768c6b2c4a4e66d9744ceeda4c656f4
John Nguyen <npjohn92@gmail.com>
2021-12-08 11:29:46 -0800

    add initial state logic

--- scripts.js
+++ scripts.js
@@ -33,3 +33,58 @@ function operate(operator, num1, num2) {
   return ans;
 }
 
+class Calculator {
+  constructor() {
+    this.firstNumber;
+    this.operator;
+    this.secondNumber;
+  }
+  updateState(val) {
+    if (typeof this.operator == 'undefined') {
+      if (/[+x\-÷]/.test(val)) {
+        this.updateOperator(val);
+      } else {
+        this.updateFirstNumber(val);
+      }
+    } else {
+      this.updateSecondNumber(val);
+    }
+  }
+  updateFirstNumber(val) {
+    if (typeof this.firstNumber == 'undefined') {
+      this.firstNumber = val;
+    } else {
+      this.firstNumber += val;
+    }
+    return this.firstNumber;
+  }
+
+  updateOperator(operator) {
+    if (this.firstNumber) {
+      this.operator = operator;
+    }
+  }
+
+  updateSecondNumber(val) {
+    if (typeof this.secondNumber == 'undefined') {
+      this.secondNumber = val;
+    } else {
+      this.secondNumber += val;
+    }
+    return this.secondNumber;
+  }
+}
+
+const calc = new Calculator();
+
+
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      // console.log(e.target.textContent);
+      calc.updateState(e.target.textContent);
+      console.log(calc);
+      // console.log(typeof e.target.textContent);
+  });
+});
