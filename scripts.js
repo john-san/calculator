ffa1c2c6f708cf377e41b440015195356c7b7fb3
John Nguyen <npjohn92@gmail.com>
2021-12-09 10:16:22 -0800

    update handleClick logic

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function add(a, b) {
 }
 
 function subtract(a,b) {
-  return a + b;
+  return a - b;
 }
 
 function multiply(a,b) {
@@ -15,6 +15,14 @@ function divide(a,b) {
 }
 
 function operate(operator, num1, num2) {
+  if (typeof num1 == 'string') {
+    num1 = parseFloat(num1);
+  }
+
+  if (typeof num2 == 'string') {
+    num2 = parseFloat(num2);
+  }
+
   let ans;
   switch (operator) {
     case '+':
@@ -26,7 +34,13 @@ function operate(operator, num1, num2) {
     case '*':
       ans = multiply(num1,num2);
       break;
+    case 'x':
+      ans = multiply(num1,num2);
+      break;
     case '/':
+      ans = divide(num1,num2);
+      break;
+    case '÷':
       ans = divide(num1,num2);
       break;
   }
@@ -35,45 +49,123 @@ function operate(operator, num1, num2) {
 
 class Calculator {
   constructor() {
-    this.firstNumber;
+    this.firstNumber = '0';
     this.operator;
     this.secondNumber;
   }
-  updateState(val) {
-    if (typeof this.operator == 'undefined') {
-      if (/[+x\-÷]/.test(val)) {
-        this.updateOperator(val);
-      } else {
+  handleClick(val) {
+    if (isANumber(val)) {
+      if (doesNotExist(this.operator)) {
         this.updateFirstNumber(val);
+      } else {
+        this.updateSecondNumber(val);
       }
     } else {
-      this.updateSecondNumber(val);
+      switch (val) {
+        case "CLEAR":
+          this.clearState();
+          break;
+        case "DELETE":
+          this.delete();
+          break;
+        case "=":
+          this.equals();
+          break;
+        case ".":
+          this.handleDecimal();
+          break;
+        default:
+          this.updateOperator(val);
+      }
     }
   }
+
   updateFirstNumber(val) {
-    if (typeof this.firstNumber == 'undefined') {
+    if (doesNotExist(this.firstNumber) || this.firstNumber == '0') {
       this.firstNumber = val;
     } else {
       this.firstNumber += val;
     }
-    return this.firstNumber;
   }
 
   updateOperator(operator) {
     if (this.firstNumber) {
-      this.operator = operator;
+      if (this.secondNumber) {
+        this.operator = operator;
+        this.equals();
+        this.operator = operator;
+      } else {
+        this.operator = operator;
+      }
     }
   }
 
   updateSecondNumber(val) {
-    if (typeof this.secondNumber == 'undefined') {
+    if (doesNotExist(this.secondNumber)) {
       this.secondNumber = val;
     } else {
       this.secondNumber += val;
     }
-    return this.secondNumber;
   }
+
+  clearState() {
+    this.firstNumber = '0';
+    this.reset('operator');
+    this.reset('secondNumber');
+  }
+
+  delete() {
+    if (doesNotExist(this.operator)) {
+      this.firstNumber = this.firstNumber.slice(0, this.firstNumber.length - 1);
+    } else {
+      this.secondNumber = this.secondNumber.slice(0, this.secondNumber.length - 1);
+    }
+  }
+
+  equals() {
+    if (this.firstNumber && this.operator && this.secondNumber) {
+      let ans = operate(this.operator, this.firstNumber, this.secondNumber);
+      this.firstNumber = ans.toString();
+      this.reset('operator');
+      this.reset('secondNumber');
+    }
+    
+  }
+
+  handleDecimal() {
+    if (this.operator) {
+      if (this.secondNumber) {
+        if (!this.secondNumber.includes('.')) {
+          this.secondNumber += '.';
+        } 
+      } else {
+        this.secondNumber = '0.';
+      }
+    } else {
+      if (!this.firstNumber.includes('.')) {
+        this.firstNumber += '.';
+      } 
+   }
+  }
+
+  reset(propName) {
+    this[propName] = undefined;
+  }
+
+  
 }
+
+
+// helpers
+function doesNotExist(prop) {
+  return typeof prop == 'undefined';
+}
+
+function isANumber(str) {
+  const numRegex = /[0-9]/;
+  return numRegex.test(str);
+}
+
 
 const calc = new Calculator();
 
@@ -83,7 +175,7 @@ const buttons = document.querySelectorAll('.btn');
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
       // console.log(e.target.textContent);
-      calc.updateState(e.target.textContent);
+      calc.handleClick(e.target.textContent);
       console.log(calc);
       // console.log(typeof e.target.textContent);
   });
